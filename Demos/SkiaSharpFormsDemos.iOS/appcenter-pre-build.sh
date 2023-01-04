4cc5a2d51361d57da0da3604d35cc9d54a788fd9
ankasani <ankasani@microsoft.com>
2023-01-04 13:43:46 +0530

    Update appcenter-pre-build.sh

--- Demos/SkiaSharpFormsDemos.iOS/appcenter-pre-build.sh
+++ Demos/SkiaSharpFormsDemos.iOS/appcenter-pre-build.sh
@@ -3,4 +3,5 @@
 set -ex
 
 sudo xcode-select --switch /Applications/Xcode_14.2.app/Contents/Developer
+echo "##vso[task.setvariable variable=MD_APPLE_SDK_ROOT;]/Applications/Xcode_14.2.app"
 xcodebuild -version
