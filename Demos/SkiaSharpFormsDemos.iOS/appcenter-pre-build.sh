5ed084446c860d2e0a589af845bbe42cdd266c69
ankasani <ankasani@microsoft.com>
2023-01-04 14:05:35 +0530

    Update appcenter-pre-build.sh

--- Demos/SkiaSharpFormsDemos.iOS/appcenter-pre-build.sh
+++ Demos/SkiaSharpFormsDemos.iOS/appcenter-pre-build.sh
@@ -4,4 +4,5 @@ set -ex
 
 sudo xcode-select --switch /Applications/Xcode_14.2.app/Contents/Developer
 echo "##vso[task.setvariable variable=MD_APPLE_SDK_ROOT;]/Applications/Xcode_14.2.app"
+$(Agent.HomeDirectory)/scripts/select-xamarin-sdk.sh 6_12_12
 xcodebuild -version
